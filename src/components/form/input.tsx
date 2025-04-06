01993fc4558569b0184d594449f037b49921189d
Ahmad-Altamimi1 <ahmad.altamimi5.1999@gmail.com>
2025-04-06 16:59:21 +0300

    SubmitHandler

---
+++ src/components/form/input.tsx
@@ -0,0 +1,20 @@
+import React, { forwardRef } from "react";
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  error?: string;
+}
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ label, error, ...props }, ref) => (
+    <div>
+      <label>{label}</label>
+      <input ref={ref} {...props} />
+      {error && <span>{error}</span>}
+    </div>
+  )
+);
+
+Input.displayName = "Input";
+
+export default Input;
