832285cbd2b765bfe86e4659df2b999b7c3a7244
SondosFikra <sondos@fikra-it.com>
2025-04-06 14:32:22 +0300

    add columns in user table

--- src/app/[locale]/(dashboard)/dashboard/customers/components/columns.tsx
+++ src/app/[locale]/(dashboard)/dashboard/customers/components/columns.tsx
@@ -27,7 +27,7 @@ import { toast } from "sonner";
 import { z } from "zod";
 import { TableCellViewer } from "@/components/table/TableCellViewer";
 import ActionCell from "@/components/table/actionCell";
-import {IResponseCustomer} from '@/entities/dashboard'
+import { IResponseCustomer } from '@/entities/dashboard'
 import { HeaderCell } from "@/components/table/headerCell";
 // export const schema = z.object({
 //   id: z.number(),
@@ -73,7 +73,7 @@ export const columns: ColumnDef<IResponseCustomer>[] = [
   },
   {
     accessorKey: "header",
-    header: "Header",
+    header: "SL",
     cell: ({ row }) => {
       return <TableCellViewer item={row.original} />;
     },
@@ -81,7 +81,7 @@ export const columns: ColumnDef<IResponseCustomer>[] = [
   },
   {
     accessorKey: "type",
-    header: "Section Type",
+    header: "Application ID",
     cell: ({ row }) => (
       <div className="w-32">
         <Badge variant="outline" className="text-muted-foreground px-1.5">
@@ -91,116 +91,76 @@ export const columns: ColumnDef<IResponseCustomer>[] = [
     ),
   },
   {
-    accessorKey: 'first_name',
-    header: <HeaderCell lable="title" />,
+    accessorKey: 'business_name',
+    header: "Business Name",
+    // header: <HeaderCell lable="title" />,
     cell: ({ row }) => (
       <div className="w-32">
         <Badge variant="outline" className="text-muted-foreground px-1.5">
-          {row.original.first_name}
+          {row.original.business_name}
         </Badge>
       </div>
     ),
   },
-  // {
-  //   accessorKey: "status",
-  //   header: "Status",
-  //   cell: ({ row }) => (
-  //     <Badge variant="outline" className="text-muted-foreground px-1.5">
-  //       {row.original.status === "Done" ? (
-  //         <IconCircleCheckFilled className="fill-green-500 dark:fill-green-400" />
-  //       ) : (
-  //         <IconLoader />
-  //       )}
-  //       {row.original.status}
-  //     </Badge>
-  //   ),
-  // },
-  // {
-  //   accessorKey: "target",
-  //   header: () => <div className="w-full text-right">Target</div>,
-  //   cell: ({ row }) => (
-  //     <form
-  //       onSubmit={(e) => {
-  //         e.preventDefault();
-  //         toast.promise(new Promise((resolve) => setTimeout(resolve, 1000)), {
-  //           loading: `Saving ${row.original.header}`,
-  //           success: "Done",
-  //           error: "Error",
-  //         });
-  //       }}
-  //     >
-  //       <Label htmlFor={`${row.original.id}-target`} className="sr-only">
-  //         Target
-  //       </Label>
-  //       <Input
-  //         className="hover:bg-input/30 focus-visible:bg-background dark:hover:bg-input/30 dark:focus-visible:bg-input/30 h-8 w-16 border-transparent bg-transparent text-right shadow-none focus-visible:border dark:bg-transparent"
-  //         defaultValue={row.original.target}
-  //         id={`${row.original.id}-target`}
-  //       />
-  //     </form>
-  //   ),
-  // },
-  // {
-  //   accessorKey: "limit",
-  //   header: () => <div className="w-full text-right">Limit</div>,
-  //   cell: ({ row }) => (
-  //     <form
-  //       onSubmit={(e) => {
-  //         e.preventDefault();
-  //         toast.promise(new Promise((resolve) => setTimeout(resolve, 1000)), {
-  //           loading: `Saving ${row.original.header}`,
-  //           success: "Done",
-  //           error: "Error",
-  //         });
-  //       }}
-  //     >
-  //       <Label htmlFor={`${row.original.id}-limit`} className="sr-only">
-  //         Limit
-  //       </Label>
-  //       <Input
-  //         className="hover:bg-input/30 focus-visible:bg-background dark:hover:bg-input/30 dark:focus-visible:bg-input/30 h-8 w-16 border-transparent bg-transparent text-right shadow-none focus-visible:border dark:bg-transparent"
-  //         defaultValue={row.original.limit}
-  //         id={`${row.original.id}-limit`}
-  //       />
-  //     </form>
-  //   ),
-  // },
-  // {
-  //   accessorKey: "reviewer",
-  //   header: "Reviewer",
-  //   cell: ({ row }) => {
-  //     const isAssigned = row.original.reviewer !== "Assign reviewer";
 
-  //     if (isAssigned) {
-  //       return row.original.reviewer;
-  //     }
-
-  //     return (
-  //       <>
-  //         <Label htmlFor={`${row.original.id}-reviewer`} className="sr-only">
-  //           Reviewer
-  //         </Label>
-  //         <Select>
-  //           <SelectTrigger
-  //             className="w-38 **:data-[slot=select-value]:block **:data-[slot=select-value]:truncate"
-  //             size="sm"
-  //             id={`${row.original.id}-reviewer`}
-  //           >
-  //             <SelectValue placeholder="Assign reviewer" />
-  //           </SelectTrigger>
-  //           <SelectContent align="end">
-  //             <SelectItem value="Eddie Lake">Eddie Lake</SelectItem>
-  //             <SelectItem value="Jamik Tashpulatov">
-  //               Jamik Tashpulatov
-  //             </SelectItem>
-  //           </SelectContent>
-  //         </Select>
-  //       </>
-  //     );
-  //   },
-  // },
   {
+    accessorKey: 'phone_number',
+    header: "Phone Number",
+    // header: <HeaderCell lable="title" />,
+    cell: ({ row }) => (
+      <div className="w-32">
+        <Badge variant="outline" className="text-muted-foreground px-1.5">
+          {row.original.phone_number}
+        </Badge>
+      </div>
+    ),
+  },
+  {
+    accessorKey: 'email',
+    header: "Email",
+    // header: <HeaderCell lable="title" />,
+    cell: ({ row }) => (
+      <div className="w-32">
+        <Badge variant="outline" className="text-muted-foreground px-1.5">
+          {row.original.phone_number}
+        </Badge>
+      </div>
+    ),
+  },
+  {
+    accessorKey: 'service',
+    header: "Services",
+    // header: <HeaderCell lable="title" />,
+    cell: ({ row }) => (
+      <div className="w-32">
+        <Badge variant="outline" className="text-muted-foreground px-1.5">
+          {row.original.phone_number}
+        </Badge>
+      </div>
+    ),
+  },
+
+  {
+    accessorKey: "status",
+    header: "Status",
+    cell: ({ row }) => (
+      <Badge variant="outline" className="text-muted-foreground px-1.5">
+        {row.original.status === "Done" ? (
+          <IconCircleCheckFilled className="fill-green-500 dark:fill-green-400" />
+        ) : (
+          <IconLoader />
+        )}
+        {row.original.status}
+      </Badge>
+    ),
+  },
+
+  {
+    accessorKey: "actions",
+    header: "Actions",
     id: "actions",
     cell: () => <ActionCell />,
   },
+  
+
 ];
