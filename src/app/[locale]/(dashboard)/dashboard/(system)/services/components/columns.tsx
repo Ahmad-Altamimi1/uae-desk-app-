20dcd2b7482ebddfb3127a83fd161fc139895d27
Ahmad-Altamimi1 <ahmad.altamimi5.1999@gmail.com>
2025-04-14 08:54:09 +0300

    enhancement types

--- src/app/[locale]/(dashboard)/dashboard/(system)/services/components/columns.tsx
+++ src/app/[locale]/(dashboard)/dashboard/(system)/services/components/columns.tsx
@@ -1,48 +1,14 @@
 "use client";
 
 import { DragHandle } from "@/components/table/dragHandle";
-import { Input } from "@/components/ui/input";
 import { Checkbox } from "@radix-ui/react-checkbox";
-
-//TODO
-// import {
-//   Select,
-//   SelectTrigger,
-//   SelectValue,
-//   SelectContent,
-//   SelectItem,
-// } from "@radix-ui/react-select";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import { IconCircleCheckFilled, IconLoader } from "@tabler/icons-react";
 import { ColumnDef } from "@tanstack/react-table";
-import { Label } from "@/components/ui/label";
-import { Badge } from "@/components/ui/badge";
-import { toast } from "sonner";
-import { z } from "zod";
-import { TableCellViewer } from "@/components/table/TableCellViewer";
 import ActionCell from "@/components/table/actionCell";
-import { IResponseUsersRoles } from '@/entities/dashboard/users'
 import { HeaderCell } from "@/components/table/headerCell";
-import { StatusCell } from "@/components/table/statusCell";
-import { IResponseServices, IResponseUsersPermissions } from "@/entities/dashboard";
+import { IServicesData } from "@/entities/dashboard";
 import { RowCell } from "@/components/table/rowCell";
-// export const schema = z.object({
-//   id: z.number(),
-//   header: z.string(),
-//   type: z.string(),
-//   status: z.string(),
-//   target: z.string(),
-//   limit: z.string(),
-//   reviewer: z.string(),
-// });
 
-export const columns: ColumnDef<IResponseServices>[] = [
+export const columns: ColumnDef<IServicesData>[] = [
   {
     id: "drag",
     header: () => null,
@@ -85,20 +51,13 @@ export const columns: ColumnDef<IResponseServices>[] = [
     ),
   },
 
-
-
   {
-
-
     accessorKey: "name",
     header: <HeaderCell label="services.name" />,
     cell: ({ row }) => (
-
       <div>
         <RowCell label={row.original.name} />
       </div>
-
-
     ),
   },
 
@@ -110,8 +69,6 @@ export const columns: ColumnDef<IResponseServices>[] = [
       <div className="flex items-center justify-center">
         <ActionCell />
       </div>
-    )
-    
+    ),
   },
-
 ];
