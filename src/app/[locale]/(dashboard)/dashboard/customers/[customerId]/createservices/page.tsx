3d45c44f02897a53a8096d0fbe96fb204451bbdf
Ahmad-Altamimi1 <ahmad.altamimi5.1999@gmail.com>
2025-04-16 20:55:58 +0300

    services

--- src/app/[locale]/(dashboard)/dashboard/customers/[customerId]/createservices/page.tsx
+++ src/app/[locale]/(dashboard)/dashboard/customers/[customerId]/createservices/page.tsx
@@ -1,60 +1,28 @@
 import React, { FC } from "react";
-import Passport from "./servicesForms/Passport";
 import { api } from "@/lib/api/serverCore";
-import { ICustomerData } from "@/entities/dashboard";
-import EmiratesId from "./servicesForms/emiratesId";
-import TaxCertificate from "./servicesForms/taxCertificate";
-import TradeLicense from "./servicesForms/tradeLicense";
-import ChamberCertificate from "./servicesForms/camberCertifcate";
-import CommercialRegister from "./servicesForms/commericalRegister";
-import PartnershipAgreement from "./servicesForms/partnershipAgreement";
-import CorporateTaxCertificate from "./servicesForms/invoice";
-import VatCertificate from "./servicesForms/vatCertifcate";
-import IncorporationCertificate from "./servicesForms/incorporation";
-import UAENationalID from "./servicesForms/uaeNational";
-import PowerOfAttorney from "./servicesForms/powe";
-import BankStatementDetails from "./servicesForms/bank";
-import LeaseAgreementDetails from "./servicesForms/leaseAgreement";
-import TrademarkCertificateDetails from "./servicesForms/tradeMark";
-import MemorandumOfAssociationDetails from "./servicesForms/assosation";
-import ShareholderAgreementDetails from "./servicesForms/shareHolderAgreement";
-import AuditedFinancialStatementDetails from "./servicesForms/financal";
-
-
-
-
+import { GroupedMediaResponse } from "@/entities/dashboard";
+import { documentComponentsMap } from "./documentComponentsMap";
+import { DocumentType } from "@/types/enums";
 interface CustomerViewProps {
   params: Promise<{ customerId: string }>;
 }
 const CreateServices: FC<CustomerViewProps> = async ({ params }) => {
-  const data = await api.get<ICustomerData>([
+  const data = await api.get<GroupedMediaResponse>([
     "groupedMedia",
     (await params).customerId,
   ]);
-  console.log("datadatadatadatadatadatadatadatadatadatadatadatadata", data);
+  console.log("Object.entries(data)", Object.entries(data.groupedMedia));
 
   return (
     <div className="grid grid-cols-2">
-      <div className="services">
-        <EmiratesId />
-        <Passport />
+      <div className="services space-y-4">
+        {Object.entries(data.groupedMedia).map(([key, value]) => {
+          const Component = documentComponentsMap[key as DocumentType];
 
-        < TaxCertificate />
-        <TradeLicense />
-        < ChamberCertificate />
-        < CommercialRegister />
-        < PartnershipAgreement />
-        < CorporateTaxCertificate />
-        <VatCertificate />
-        < IncorporationCertificate />
-        < UAENationalID />
-        <PowerOfAttorney />
-        <BankStatementDetails />
-        <LeaseAgreementDetails />
-        <TrademarkCertificateDetails />
-        <MemorandumOfAssociationDetails />
-        < ShareholderAgreementDetails />
-        <AuditedFinancialStatementDetails/>
+          console.log("ComponentComponentComponent", key);
+
+          return value.map((_, index) => <Component key={`${key}-${index}`} />);
+        })}
       </div>
       <iframe
         src={`https://docs.google.com/gview?url=${`https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf`}&embedded=true`}
